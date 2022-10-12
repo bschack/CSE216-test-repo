12b2e686b09d4814f4c4c859484c83a49f9a3d9e
bschack <benharris1102@gmail.com>
2022-10-12 14:35:34 -0400

    upgrade: added post listing to homepage

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,12 +1,29 @@
+import { PostCard } from "../../libs/components/postCard/postCard";
 import { PostForm } from "../../libs/components/postForm/postForm";
+import { testPost } from "../../libs/constants/constants";
 import { Section } from "../../libs/content/section/section";
 
 const Homepage = () => {
-    return (
-        <Section>
-            <PostForm />
-        </Section>
-    );
+  const posts = testPost;
+
+  const cardedPosts = posts.map((post) => (
+    <PostCard
+      key={post.id}
+      id={post.id}
+      message={post.message}
+      likes={post.likes}
+      date={post.date}
+    />
+  ));
+
+  return (
+    <Section space>
+      <Section>
+        <PostForm />
+      </Section>
+      <Section posts>{cardedPosts}</Section>
+    </Section>
+  );
 };
 
 export default Homepage;
