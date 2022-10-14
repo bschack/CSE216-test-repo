21a8d773ba0a8b957ba63e73ee0184e0efc761fa
bschack <benharris1102@gmail.com>
2022-10-14 15:49:37 -0400

    feature: added profile page support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,40 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AboutPage } from "./about/about";
+import {
+  BrowserRouter as Router,
+  Link,
+  Navigate,
+  Route,
+  Routes
+} from "react-router-dom";
+import { LoginPage } from "./login/login";
 import Homepage from "./homepage/homepage";
 // import { NavBar } from '../libs/content/navBar/navBar';
 import "../libs/styles/lib/globals.scss";
 import { Footer } from "../libs/content/footer/footer";
+import { useState } from "react";
+import { googleUserType } from "../libs/constants/types";
 
 const App = () => {
+  const [user, setUser] = useState<googleUserType>();
+
+  const uuid = user?.email.split("@")[0];
+
   return (
     <Router>
+      <Link to="/home">Home</Link>
+      <Link to="/profile">Profile</Link>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/*"
+          element={user ? <Navigate to="/home" /> : <Navigate to="/login" />}
+        />
+        {user ? (
+          <>
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/profile" element={uuid} />
+          </>
+        ) : (
+          <Route path="/login" element={<LoginPage login={setUser} />} />
+        )}
       </Routes>
       <Footer />
     </Router>
