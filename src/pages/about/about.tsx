6e5cf303ccfe9f249bbfe877ace394a6012ad65d
bschack <benharris1102@gmail.com>
2022-10-14 15:48:52 -0400

    fix: remove about page

--- src/pages/about/about.tsx
+++
@@ -1,9 +0,0 @@
-import { Section } from "../../libs/content/section/section";
-
-export const AboutPage = () => {
-    return (
-        <Section>
-            About page
-        </Section>
-    );
-};
