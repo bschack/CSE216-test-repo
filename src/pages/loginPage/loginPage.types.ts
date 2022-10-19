49c81765267579273495d5fdd0bcef8131fedb0f
bschack <benharris1102@gmail.com>
2022-10-19 19:01:46 -0400

    update: added supported types

--- src/pages/loginPage/loginPage.types.ts
+++ src/pages/loginPage/loginPage.types.ts
@@ -1,5 +1,30 @@
 import { Dispatch } from "react";
 
 export type LoginPageProps = {
-  login: Dispatch<React.SetStateAction<boolean>>;
+  login: Dispatch<React.SetStateAction<string>>;
 };
+
+export type googleUserType = {
+  aud: string;
+  azp: string;
+  email: string;
+  email_verified: boolean;
+  exp: number;
+  family_name: string;
+  given_name: string;
+  hd: string;
+  iat: number;
+  iss: string;
+  jti: string;
+  name: string;
+  nbf: number;
+  picture: string;
+  sub: string;
+};
+
+export type sessionProps =
+  | {
+      sessionHash: string;
+      status: string;
+    }
+  | undefined;
