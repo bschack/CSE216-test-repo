6dc0e2cf29907aa580fbf3220a59f9c38ee6f806
bschack <benharris1102@gmail.com>
2022-10-19 19:52:22 -0400

    feature: added profile page to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,43 +13,47 @@ import { Footer } from "../libs/content/footer/footer";
 import { useEffect, useState } from "react";
 import { alertProps, loginSuccessAlert } from "../libs/constants/constants";
 import { AlertBox } from "../libs/components/alert/alert";
+import { ProfilePage } from "./profilePage/profilePage";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState<string>("");
   // const uuid = user?.email.split("@")[0];
-  const c = window.sessionStorage.getItem("shk") || "";
+  const shk = window.sessionStorage.getItem("shk") || "";
+  const uid = window.sessionStorage.getItem("uid") || "";
 
   const [alertsList, setAlertsList] = useState<alertProps[]>([]);
   const { addAlert, Alerts } = AlertBox(alertsList, setAlertsList);
 
   useEffect(() => {
-    if (c !== "") {
-      setLoggedIn(c);
+    if (shk !== "") {
+      setLoggedIn(shk);
       addAlert(loginSuccessAlert);
     } else {
       setLoggedIn("");
     }
-  }, [addAlert, c]);
+  }, [addAlert, shk]);
 
   return (
     <Router>
       <Link to="/home">Home</Link>
-      <Link to="/profile">Profile</Link>
+      <Link to={`/profile`}>Profile</Link>
+      <Link to={`/profile/9999`}>Test invalid profile</Link>
       <Routes>
-        <Route
-          path="*"
-          element={
-            loggedIn !== "" ? <Navigate to="/home" /> : <Navigate to="/login" />
-          }
-        />
+        <Route path="/profile" element={<Navigate to={`/profile/${uid}`} />} />
         {loggedIn !== "" ? (
           <>
             <Route path="/home" element={<Homepage />} />
-            <Route path="/profile" element={"Profile page"} />
+            <Route path="/profile/:uid" element={<ProfilePage />} />
           </>
         ) : (
           <Route path="/login" element={<LoginPage login={setLoggedIn} />} />
         )}
+        <Route
+          path="/*"
+          element={
+            loggedIn !== "" ? <Navigate to="/home" /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
       <Footer />
       <Alerts />
