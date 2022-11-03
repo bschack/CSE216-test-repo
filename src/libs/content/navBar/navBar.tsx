042c4af7ad755cdf42a08a0a3b29eaf41a701c9d
bschack <benharris1102@gmail.com>
2022-11-03 16:32:01 -0400

    feature: navbar revamp, reactive to page

--- src/libs/content/navBar/navBar.tsx
+++ src/libs/content/navBar/navBar.tsx
@@ -1,21 +1,52 @@
-import { Link } from 'react-router-dom';
-import styles from './navBar.module.scss';
+import clsx from "clsx";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./navBar.module.scss";
 
 export const NavBar = () => {
-    return (
-        <div className={styles['navBar']}>
-            <div className={styles['navBar__section']}>
-                <Link to='/'>
-                    <div className={styles['navBar__pill']}>
-                        Launch Page
-                    </div>
-                </Link>
-                <Link to='/about'>
-                    <div className={styles['navBar__pill']}>
-                        About
-                    </div>
-                </Link>
-            </div>
-        </div>
-    );
+  const path = useLocation().pathname;
+  const home = path === "/home";
+  const profile = path.indexOf("/profile") > -1;
+  const comments = path.indexOf("/post") > -1;
+
+  return (
+    <div className={styles["navBar"]}>
+      <div className={styles["navBar__section"]}>
+        <Link to="/home">
+          <div className={styles["navBar__logo"]}>
+            Blen<div>d</div>
+          </div>
+        </Link>
+      </div>
+      <div className={styles["navBar__button-group"]}>
+        {comments ? (
+          <div
+            className={clsx(
+              styles["navBar__link-active"],
+              styles["navBar__link"]
+            )}
+          >
+            Comments
+          </div>
+        ) : null}
+        <Link
+          to="/home"
+          className={clsx(
+            styles["navBar__link"],
+            home ? styles["navBar__link-active"] : null
+          )}
+        >
+          Home
+        </Link>
+        <Link
+          to="/profile"
+          className={clsx(
+            styles["navBar__link"],
+            profile ? styles["navBar__link-active"] : null
+          )}
+        >
+          Profile
+        </Link>
+      </div>
+    </div>
+  );
 };
