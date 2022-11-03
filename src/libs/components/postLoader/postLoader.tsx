44ee2c7ab52a4e72561c806aa6bf831515dddb64
bschack <benharris1102@gmail.com>
2022-11-03 16:28:03 -0400

    update: added size prop

--- src/libs/components/postLoader/postLoader.tsx
+++ src/libs/components/postLoader/postLoader.tsx
@@ -1,8 +1,10 @@
+import clsx from "clsx";
 import styles from "./postLoader.module.scss";
+import { postLoaderProps } from "./postLoader.types";
 
-export const PostLoader = () => {
+export const PostLoader = ({ size = "s" }: postLoaderProps) => {
   return (
-    <div className={styles["post-loader"]}>
+    <div className={clsx(styles["post-loader"], styles[`post-loader-${size}`])}>
       <div className={styles["post-loader__spinner"]}></div>
       <div className={styles["post-loader__cutout"]}></div>
     </div>
---
+++ src/libs/components/postLoader/postLoader.types.ts
@@ -0,0 +1,3 @@
+export type postLoaderProps = {
+  size?: "s" | "l";
+};
