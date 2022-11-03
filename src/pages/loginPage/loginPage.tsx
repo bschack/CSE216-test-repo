8161c6dcb6574d758faa9f667fb1cf3994c7bb35
bschack <benharris1102@gmail.com>
2022-11-03 16:33:23 -0400

    feature: login page style v2

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   GOOGLE_CLIENT_ID,
   loginFailedAlert,
   loginSuccessAlert
 } from "../../libs/constants/constants";
-import { Section } from "../../libs/content/section/section";
 import { loginUser } from "../../libs/hooks/loginUser";
 import { LoginPageProps, sessionProps } from "./loginPage.types";
 
+import styles from "./loginPage.module.scss";
+import clsx from "clsx";
+
 export const LoginPage = ({ login, alerts }: LoginPageProps) => {
+  const [googleLogin, setGoogleLogin] = useState<boolean>(false);
+
   const handleCredentialResponse = async (res: any) => {
     const credential = res.credential;
+    console.log(credential);
 
     await loginUser(credential)
       .then((sh: sessionProps) => {
@@ -29,25 +34,51 @@ export const LoginPage = ({ login, alerts }: LoginPageProps) => {
   };
 
   useEffect(() => {
+    document.title = "Login | BLEND";
     // @ts-ignore: Cannot find name
     google.accounts.id.initialize({
       client_id: GOOGLE_CLIENT_ID,
       callback: handleCredentialResponse
     });
     // @ts-ignore: Cannot find name
-    google.accounts.id.renderButton(document.getElementById("buttonDiv"), {
+    google.accounts.id.renderButton(document.getElementById("google-login"), {
       theme: "filled_black",
-      size: "large",
-      shape: "pill",
-      logo_alignment: "left"
+      size: "large"
     });
     // @ts-ignore: Cannot find name
-    google.accounts.id.prompt();
+    // google.accounts.id.prompt();
   });
 
   return (
-    <Section space>
-      <div id="buttonDiv"></div>
-    </Section>
+    <main className={styles["login-page__container"]}>
+      <div className={styles["login-page__logo"]}>Blend</div>
+      <div className={styles["login-page__logo"]}>Blend</div>
+      <div className={styles["login-page__logo"]}>Blend</div>
+      <div className={styles["login-page__button-group"]}>
+        <div
+          className={styles["login-page__button-hover"]}
+          onClick={() => {
+            if (!googleLogin) setGoogleLogin(true);
+            else setGoogleLogin(false);
+          }}
+        >
+          Login
+        </div>
+        <div
+          id="google-login"
+          className={clsx(
+            styles["login-page__button-google"],
+            googleLogin ? styles["login-page__button-google-active"] : null
+          )}
+        >
+          Hello
+        </div>
+      </div>
+      <div className={styles["lines"]}>
+        <div className={styles["line"]}></div>
+        <div className={styles["line"]}></div>
+        <div className={styles["line"]}></div>
+      </div>
+    </main>
   );
 };
